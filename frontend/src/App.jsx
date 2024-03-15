2796d174628cb77868b1be995974d92a0efa93c2
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-03-15 11:09:40 -0300

    add Login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Login from './components/Login/Login'
 
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>No te la delires app</h1>
+      <Login/>
     </>
   )
 }
