40e20eb96dab524a7f245c0e33b9c3f8608eeefb
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2024-04-14 19:03:14 -0300

    add middleware auth

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,19 @@
+// middleware/auth.middleware.js
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token.' });
+  }
+};
